b3944460afa141daefaa91d14089a347115a9d5e
KimC <kconn029@fiu.edu>
2023-03-13 19:41:00 -0400

    Initial Commit

---
+++ BeReal/Extensions/ViewController+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController+Extensions.swift
+//
+//
+//  Created by Kim Connolly on 3/12/23.
+//
+
+import UIKit
+
+extension UIViewController {
+
+    /// Shows an alert with the given description or default text if nil.
+    func showAlert(description: String? = nil) {
+        let alertController = UIAlertController(title: "Oops...", message: "\(description ?? "Unknown error")", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+}
